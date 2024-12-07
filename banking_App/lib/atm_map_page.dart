f3886b7bb65320ad911408e7a20cb7a341ca38f1
zackschottmann <zschottmann5144@conestoga.on.ac>
2024-12-07 17:53:28 -0500

    interacting map

--- banking_App/lib/atm_map_page.dart
+++ banking_App/lib/atm_map_page.dart
@@ -3,30 +3,39 @@ import 'package:syncfusion_flutter_maps/maps.dart';
 
 class ATMMapPage extends StatelessWidget {
   final List<MapLatLng> atmLocations = [
-    MapLatLng(37.7749, -122.4194), // Example: San Francisco
-    MapLatLng(34.0522, -118.2437), // Example: Los Angeles
-    MapLatLng(40.7128, -74.0060),  // Example: New York City
+    MapLatLng(43.4643, -80.5204), 
+    MapLatLng(43.4668, -80.5222), 
+    MapLatLng(43.4503, -80.5255), 
+    MapLatLng(43.4743, -80.5438), 
+    MapLatLng(43.4621, -80.4955),
+    MapLatLng(43.4535, -80.4920), 
+    MapLatLng(43.4823, -80.5442), 
+    MapLatLng(43.4750, -80.5127), 
+    MapLatLng(43.4617, -80.5209),
+    MapLatLng(43.4562, -80.5075),
   ];
+
+ ATMMapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ATM Locations'),
+        title: const Text('ATM Locations'),
       ),
       body: SfMaps(
         layers: [
           MapTileLayer(
             urlTemplate:
                 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OpenStreetMap tiles
-            initialZoomLevel: 5,
-            initialFocalLatLng: MapLatLng(37.7749, -122.4194), // Center map on a location
+            initialZoomLevel: 13,
+            initialFocalLatLng: const MapLatLng(43.4643, -80.5204), // Center map on a location
             zoomPanBehavior: MapZoomPanBehavior(),
             markerBuilder: (BuildContext context, int index) {
               return MapMarker(
                 latitude: atmLocations[index].latitude,
                 longitude: atmLocations[index].longitude,
-                child: Icon(
+                child: const Icon(
                   Icons.location_on,
                   color: Colors.red,
                   size: 30,
--- banking_App/lib/models/atm_map_page.dart
+++ banking_App/lib/models/atm_map_page.dart
@@ -3,16 +3,18 @@ import 'package:syncfusion_flutter_maps/maps.dart';
 
 class ATMMapPage extends StatelessWidget {
   final List<MapLatLng> atmLocations = [
-    MapLatLng(37.7749, -122.4194), // Example: San Francisco
-    MapLatLng(34.0522, -118.2437), // Example: Los Angeles
-    MapLatLng(40.7128, -74.0060),  // Example: New York City
+    const MapLatLng(37.7749, -122.4194), // Example: San Francisco
+    const MapLatLng(34.0522, -118.2437), // Example: Los Angeles
+    const MapLatLng(40.7128, -74.0060),  // Example: New York City
   ];
+
+ATMMapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ATM Locations'),
+        title: const Text('ATM Locations'),
       ),
       body: SfMaps(
         layers: [
@@ -20,13 +22,13 @@ class ATMMapPage extends StatelessWidget {
             urlTemplate:
                 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OpenStreetMap tiles
             initialZoomLevel: 5,
-            initialFocalLatLng: MapLatLng(37.7749, -122.4194), // Center map on a location
+            initialFocalLatLng: const MapLatLng(37.7749, -122.4194), // Center map on a location
             zoomPanBehavior: MapZoomPanBehavior(),
             markerBuilder: (BuildContext context, int index) {
               return MapMarker(
                 latitude: atmLocations[index].latitude,
                 longitude: atmLocations[index].longitude,
-                child: Icon(
+                child: const Icon(
                   Icons.location_on,
                   color: Colors.red,
                   size: 30,
