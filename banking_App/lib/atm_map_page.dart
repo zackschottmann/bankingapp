110b70a991c14683a76017420e42b9444310606f
zackschottmann <zschottmann5144@conestoga.on.ac>
2024-12-08 15:34:28 -0500

    updated atm_map_page

--- banking_App/lib/atm_map_page.dart
+++ banking_App/lib/atm_map_page.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_maps/maps.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ATMMapPage extends StatelessWidget {
-  final List<MapLatLng> atmLocations = [
-    MapLatLng(43.4643, -80.5204), 
-    MapLatLng(43.4668, -80.5222), 
-    MapLatLng(43.4503, -80.5255), 
-    MapLatLng(43.4743, -80.5438), 
-    MapLatLng(43.4621, -80.4955),
-    MapLatLng(43.4535, -80.4920), 
-    MapLatLng(43.4823, -80.5442), 
-    MapLatLng(43.4750, -80.5127), 
-    MapLatLng(43.4617, -80.5209),
-    MapLatLng(43.4562, -80.5075),
+class ATMMapPage extends StatefulWidget {
+  const ATMMapPage({super.key});
+
+  @override
+  _ATMMapPageState createState() => _ATMMapPageState();
+}
+
+class _ATMMapPageState extends State<ATMMapPage> {
+  late GoogleMapController _mapController;
+
+  // List of ATM locations around Waterloo, Ontario
+  final List<LatLng> atmLocations = [
+    const LatLng(43.4643, -80.5204), // Waterloo City Center
+    const LatLng(43.4668, -80.5222), // University of Waterloo
+    const LatLng(43.4503, -80.5255), // Wilfrid Laurier University
+    const LatLng(43.4743, -80.5438), // RIM Park
+    const LatLng(43.4621, -80.4955), // Bechtel Park
+    const LatLng(43.4535, -80.4920), // Conestoga Mall
+    const LatLng(43.4823, -80.5442), // Grey Silo Golf Club
+    const LatLng(43.4750, -80.5127), // Laurel Creek Conservation Area
+    const LatLng(43.4617, -80.5209), // Bauer Marketplace
+    const LatLng(43.4562, -80.5075), // Waterloo Public Library
   ];
 
- ATMMapPage({super.key});
+  // List of markers for the ATMs
+  final Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Add markers for each ATM location
+    for (int i = 0; i < atmLocations.length; i++) {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('atm_$i'),
+          position: atmLocations[i],
+          infoWindow: InfoWindow(
+            title: 'ATM ${i + 1}',
+            snippet: 'Location ${i + 1}',
+          ),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,28 +54,15 @@ class ATMMapPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ATM Locations'),
       ),
-      body: SfMaps(
-        layers: [
-          MapTileLayer(
-            urlTemplate:
-                'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OpenStreetMap tiles
-            initialZoomLevel: 13,
-            initialFocalLatLng: const MapLatLng(43.4643, -80.5204), // Center map on a location
-            zoomPanBehavior: MapZoomPanBehavior(),
-            markerBuilder: (BuildContext context, int index) {
-              return MapMarker(
-                latitude: atmLocations[index].latitude,
-                longitude: atmLocations[index].longitude,
-                child: const Icon(
-                  Icons.location_on,
-                  color: Colors.red,
-                  size: 30,
-                ),
-              );
-            },
-            initialMarkersCount: atmLocations.length,
-          ),
-        ],
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(43.4643, -80.5204), // Centered on Waterloo
+          zoom: 13, // Zoom level for city view
+        ),
+        markers: _markers,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
       ),
     );
   }
